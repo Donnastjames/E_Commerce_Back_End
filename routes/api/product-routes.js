bd63305ebe71749e97b4b479eee5a47cb771ce68
Donna <donnastjames@comcast.net>
2021-06-20 23:50:22 -0700

    product-routes - find all products works now

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productName = await Product.findAll({
+      // be sure to include its associated Category and Tag data
+      include: [{ model: Category }]
+    });
+
+    if(!productName) {
+      res.status(404).json({ message: 'No products were found!'});
+      return;
+    }
+
+    res.status(200).json(productName);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
