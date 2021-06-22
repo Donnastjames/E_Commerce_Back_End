4118dcc7a8f672526a1f87a2126492254155a35b
Donna <donnastjames@comcast.net>
2021-06-22 00:17:28 -0700

    tag-routes DELETE an existing tag by ID is working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -82,6 +82,22 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagName = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagName) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagName);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
