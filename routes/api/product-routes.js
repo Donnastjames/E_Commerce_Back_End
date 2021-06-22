ae5a300516cd0ab1a8eed72c2997fb52c9210933
Donna <donnastjames@comcast.net>
2021-06-22 14:17:40 -0700

    product-routes DELETE an existing product by ID is working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,8 +116,24 @@ router.put('/:id', (req, res) => {
   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productName = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productName) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productName);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
