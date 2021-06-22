735afe026fcf8579330dbd27440c1ae366143e43
Donna <donnastjames@comcast.net>
2021-06-21 23:54:48 -0700

    tag-routes POST / Create a new tag is working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,6 +57,12 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagName = await Tag.create(req.body);
+    res.status(200).json(tagName);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
